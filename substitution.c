f39c73c9469219e4d37b36a1dff8238b29faa833
jcliese <jcliese@users.noreply.github.com>
2020-07-20 21:17:42 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -3,16 +3,20 @@
 #include <string.h>
 #include <ctype.h>
 
-
+//initialization of functions
 int check_key(string);
 string encipher(string, string);
+//begin main function
 int main(int argc, char *argv[])
 {
+    //getting commmand line argument
     string key = argv[1];
     int code;
     string plaintext = "";
+    //check for right amount of arguments
     if (argc == 2)
     {
+        //call key check method
         code = check_key(argv[1]);
         if (code == 0)
         {
@@ -25,6 +29,7 @@ int main(int argc, char *argv[])
             return 1;
         }
     }
+    //handle wrong amount of arguments
     else
     {
         printf("Usage: ./substitution key\n");
@@ -32,6 +37,7 @@ int main(int argc, char *argv[])
     }
 }
 
+//checking key
 int check_key(string key)
 {
     char alpha[] = "abcdefghijklmnopqrstuvwxyz";
@@ -39,8 +45,10 @@ int check_key(string key)
     {
         key[i] = tolower(key[i]);
     }
+    //check key for right length
     if (strlen(key) == 26)
     {
+        //check key for duplicates
         for (int i = 0; i < strlen(alpha); i++)
         {
             char *position_ptr = strchr(key, alpha[i]);
@@ -62,24 +70,28 @@ int check_key(string key)
     }
 }
 
+//encipher function
 string encipher(string key, string plaintext)
 {
     int i = 0;
     char alpha[] = "abcdefghijklmnopqrstuvwxyz";
     while (plaintext[i] != '\0')
     {
+        //encipher all lower letters
         if (plaintext[i] >= 'a' && plaintext[i] <= 'z')
         {
             char *position_ptr = strchr(alpha, plaintext[i]);
             int position = (position_ptr == NULL ? -1 : position_ptr - alpha);
             plaintext[i] = key[position];
         }
+        //encifer all capitalized letters
         else if (plaintext[i] >= 'A' && plaintext[i] <= 'Z')
         {
             char *position_ptr = strchr(alpha, tolower(plaintext[i]));
             int position = (position_ptr == NULL ? -1 : position_ptr - alpha);
             plaintext[i] = toupper(key[position]);
         }
+        //keep all other symbols
         else
         {
             plaintext[i] = plaintext[i];

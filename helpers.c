35f52dcf8513c409a366b504edd64320d7c94496
jcliese <jcliese@users.noreply.github.com>
2020-08-16 15:53:56 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -47,14 +47,35 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    /*for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j] = copy[i][j];
+            float blue = 0, green = 0, red = 0;
+            int count = 0;
+            for (int k = -1; k < 2; k++)
+            {
+                for (int l = -1; l < 2; l++)
+                {
+                    if ((i + k < 0 || i + k >= height) || (j + l < 0 || j + l >= width))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        blue += image[i + k][j + l].rgbtBlue;
+                        green += image[i + k][j + l].rgbtGreen;
+                        red += image[i + k][j + l].rgbtRed;
+                        count++;
+                    }
+                }
+            }
+            image[i][j].rgbtBlue = round(blue/count);
+            image[i][j].rgbtGreen = round(green/count);
+            image[i][j].rgbtRed = round(red/count);
         }
     }
-    return;*/
+    return;
 }
 
 // Detect edges

04a3ff58b5cbafa09583d3beed7d05894d17c441
jcliese <jcliese@users.noreply.github.com>
2020-08-17 08:39:57 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -47,6 +47,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -65,9 +73,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     }
                     else
                     {
-                        blue += image[h][w].rgbtBlue;
-                        green += image[h][w].rgbtGreen;
-                        red += image[h][w].rgbtRed;
+                        blue += copy[h][w].rgbtBlue;
+                        green += copy[h][w].rgbtGreen;
+                        red += copy[h][w].rgbtRed;
                         count++;
                         if (i == 1 && j == 1)
                         {

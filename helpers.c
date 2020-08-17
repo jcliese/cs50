c8d7a29895694a243e1085da1c52d4030dc2c8d9
jcliese <jcliese@users.noreply.github.com>
2020-08-17 10:08:26 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -92,5 +92,114 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sobel_blue = 0, sobel_green = 0, sobel_red = 0;
+            float blue = 0, green = 0, red = 0;
+            int Gx_blue = 0, Gx_green = 0, Gx_red = 0;
+            int Gy_blue = 0, Gy_green = 0, Gy_red = 0;
+            for (int k = -1; k < 2; k++)
+            {
+                for (int l = -1; l < 2; l++)
+                {
+                    int multi;
+                    int h = i + k;
+                    int w = j + l;
+                    if ((h < 0 || h >= height) || (w < 0 || w >= width))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (l == -1 || l == 1)
+                        {
+                            switch (k)
+                            {
+                                case -1:
+                                case 1:
+                                    multi = l;
+                                    break;
+                                case 0:
+                                    multi = l * 2;
+                                    break;
+                                default:
+                                    multi = 0;
+                            }
+                        }
+                        else
+                        {
+                            multi = 0;
+                        }
+                        Gx_blue += (copy[h][w].rgbtBlue * multi);
+                        Gx_green += (copy[h][w].rgbtGreen * multi);
+                        Gx_red += (copy[h][w].rgbtRed * multi);
+                    }
+                }
+                for (int l = -1; l < 2; l++)
+                {
+                    int multi;
+                    int h = i + k;
+                    int w = j + l;
+                    if ((h < 0 || h >= height) || (w < 0 || w >= width))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (k == -1 || k == 1)
+                        {
+                            switch (l)
+                            {
+                                case -1:
+                                case 1:
+                                    multi = k;
+                                    break;
+                                case 0:
+                                    multi = k * 2;
+                                    break;
+                                default:
+                                    multi = 0;
+                            }
+                        }
+                        else
+                        {
+                            multi = 0;
+                        }
+                        Gy_blue += (copy[h][w].rgbtBlue * multi);
+                        Gy_green += (copy[h][w].rgbtGreen * multi);
+                        Gy_red += (copy[h][w].rgbtRed * multi);
+                    }
+                }
+            }
+            sobel_blue = round(sqrt(pow(Gx_blue, 2) + pow(Gy_blue, 2)));
+            sobel_green = round(sqrt(pow(Gx_green, 2) + pow(Gy_green, 2)));
+            sobel_red = round(sqrt(pow(Gx_red, 2) + pow(Gy_red, 2)));
+            if (sobel_blue > 255)
+            {
+                sobel_blue = 255;
+            }
+            if (sobel_green > 255)
+            {
+                sobel_green = 255;
+            }
+            if (sobel_red > 255)
+            {
+                sobel_red = 255;
+            }
+            image[i][j].rgbtBlue = sobel_blue;
+            image[i][j].rgbtGreen = sobel_green;
+            image[i][j].rgbtRed = sobel_red;
+        }
+    }
     return;
 }

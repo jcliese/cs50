10cc1cdaacb561ae411a2a5d7152bf32f0cfcea8
jcliese <jcliese@users.noreply.github.com>
2020-08-19 10:59:24 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -4,14 +4,18 @@
 
 int main(int argc, char *argv[])
 {
+    //check for right amount of command-line arguments
     if (argc != 2)
     {
+        //print help
         printf("Usage: ./recover image\n");
         return 1;
     }
     else
     {
+        //open file for reading with command-line argument
         FILE *file = fopen(argv[1], "r");
+        //check for opening error
         if (file == NULL)
         {
             printf("Unable to open file.");
@@ -19,15 +23,19 @@ int main(int argc, char *argv[])
         }
         else
         {
+            //init byte type
             typedef uint8_t BYTE;
             BYTE buf[512];
             int count = 0;
             char filename[8];
             FILE *img = NULL;
-            while(fread(buf, sizeof(BYTE), 512, file) == 512)
+            //read file in 512 byte chunks
+            while (fread(buf, sizeof(BYTE), 512, file) == 512)
             {
+                //check for new jpeg
                 if (buf[0] == 0xff && buf[1] == 0xd8 && buf[2] == 0xff && (buf[3] & 0xf0) == 0xe0)
                 {
+                    //check whether init jpeg or additional
                     if (count == 0)
                     {
                         sprintf(filename, "%03i.jpg", count);
@@ -54,13 +62,16 @@ int main(int argc, char *argv[])
                         count++;
                     }
                 }
+                //append data to jpeg
                 else if (count > 0)
                 {
                     fwrite(buf, sizeof(BYTE), 512, img);
                 }
             }
+            //close open img file
             fclose(img);
         }
+        //close open reading file
         fclose(file);
     }
 
